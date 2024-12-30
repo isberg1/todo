7cbb4470c7168fdc13770b37cfd576d4370a8ff0
isberg1 <isberg1_github@outlook.com>
2024-12-30 15:00:03 +0100

    improve settings

--- src/code/Settings.tsx
+++ src/code/Settings.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import { Setter, Setting } from './type';
@@ -23,7 +25,28 @@ const textSizes = [
 
 export function Settings({ setting, setSetting }: Props) {
   const [show, setShow] = useState(false);
+  const [sliderValue, setSetSliderValue] = useState(() => textSizes.findIndex((ele, idx) => {
+    if (ele === setting.textSize) {
+      return true;
+    }
+    return false;
+  }));
   const dialogEle = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const idx = textSizes.findIndex((ele) => {
+      if (ele === setting.textSize) {
+        return true;
+      }
+      return false;
+    });
+    setSetSliderValue(idx);
+  }, [setting.textSize]);
+
+  useEffect(() => {
+    setShow(true);
+    dialogEle.current?.showModal();
+  }, []);
 
   // set global styles from settings
   useEffect(() => {
@@ -34,7 +57,7 @@ export function Settings({ setting, setSetting }: Props) {
   return (
     <>
       <button
-        className={classNames('p-1  flex justify-center items-center', {
+        className={classNames('p-1 flex justify-center items-center', {
           [setting.theme.textColor]: true,
         })}
         onClick={() => {
@@ -46,18 +69,30 @@ export function Settings({ setting, setSetting }: Props) {
       </button>
 
       <dialog
+        onClick={(event) => {
+          const rect = dialogEle.current?.getBoundingClientRect();
+          if (!rect) return;
+
+          const isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+            rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+
+          if (!isInDialog) {
+            setShow(false);
+            dialogEle.current?.close();
+          }
+        }}
         ref={dialogEle}
         open={show}
-        className={classNames('max-w-[95vw] border-white rounded border-2 scroll-mt-4', {
+        className={classNames('max-w-[95vw] border-white rounded border-2 scroll-mt-4 backdrop:bg-black/50 backdrop:backdrop-blur-[1px]', {
           [setting.theme.bg]: true,
           [setting.theme.textColor]: true,
         })}
       >
 
         <div className='w-[85vw] md:w-[60vw] h-full p-1 flex flex-col justify-center items-center gap-1'>
-          <div className='w-full flex justify-end sticky top-0 h-9'>
+          <div className='w-full flex justify-end sticky top-0'>
             <button
-              className='right-0 mr-2 '
+              className='right-0 mr-2 rounded-full px-2'
               onClick={() => {
                 setShow(false);
                 dialogEle.current?.close();
@@ -68,13 +103,38 @@ export function Settings({ setting, setSetting }: Props) {
 
           </div>
           <h1 className='-mt-1 underline'>Settings</h1>
-          {textSizes.map((val) => (
-            <button
-              className='shadow px-2 min-h-11'
-              onClick={() => setSetting((old) => ({ ...old, textSize: val }))}
-            >{val}
-            </button>
-          ))}
+
+          <div className=' min-h-9 w-full flex flex-col justify-center items-center gap-2 mb-3'>
+            <div className='w-full flex flex-col justify-center items-center'>
+              <span>Text Size: {sliderValue}</span>
+              <input
+                className='w-[90%]  md:w-9/12'
+                type='range'
+                min='0'
+                max={textSizes.length - 1}
+                value={sliderValue}
+                onChange={(e) => {
+                  setSetSliderValue(Number(e.target.value));
+                  setSetting((old) => ({ ...old, textSize: textSizes[Number(e.target.value)] }));
+                }}
+              />
+            </div>
+
+            <div className='w-full flex flex-col justify-center items-center'>
+              <button
+                className='border-solid border-white border-2 px-2 rounded'
+                onClick={() => {
+                  try {
+                    localStorage.clear();
+                    window.location.reload();
+                  } catch (error) { }
+                }}
+              >
+                Reset storage
+              </button>
+            </div>
+          </div>
+
         </div>
 
       </dialog>
