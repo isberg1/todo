836436b3f9470df22fa11a3a0b04b33a8f22e2e0
isberg1 <isberg1_github@outlook.com>
2024-12-30 12:57:54 +0100

    test better longClick for touch

--- src/code/Form.tsx
+++ src/code/Form.tsx
@@ -19,8 +19,10 @@ export function Form({
   list, setList, setting,
 }: Props) {
   const [input, setInput] = useState(defaultItem);
-  const [deleteActive, setDeleteActive] = useState(false);
-  const canDelete = useRef(false);
+  const [downActive, setDownActive] = useState(false);
+  const id = useRef<ReturnType<typeof setTimeout>>();
+
+  const [deleteObj, setDeleteObj] = useState<Item[]>();
 
   const buttonState = useMemo(() => {
     if (list.some((item) => item.state === 'edit')) {
@@ -44,106 +46,106 @@ export function Form({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [buttonState]);
 
-  const onSubmit = useCallback((e: SyntheticEvent) => {
-    e.preventDefault();
-    let resetInput = false;
-
-    switch (buttonState) {
-      case 'edit': {
-        setList((old) => {
-          const idx = old.findIndex((item) => item.id === input.id);
-          if (idx !== -1) {
-            resetInput = true;
-            const tmp = [...old];
-            tmp[idx] = input;
-            tmp[idx].state = 'show';
-
-            return tmp;
-          }
-          return old;
-        });
-        break;
-      }
-      case 'delete': {
-        if (canDelete.current) {
-          setList((old) => old.filter((item) => item.state !== 'delete'));
-          canDelete.current = false;
-        }
-        break;
-      }
-      case 'add': {
-        if (input) {
-          setList((old) => [{
-            name: input.name, quantity: 1, state: 'show', id: `${input}${Date.now()}`,
-          }, ...old]);
-
-          resetInput = true;
-        }
-        break;
-      }
-    }
-    if (resetInput) {
-      setInput(defaultItem);
-    }
-  }, [buttonState, input, setList]);
-
-  const activateDel = useCallback(() => {
-    if (buttonState === 'delete') {
-      setDeleteActive(true);
-    }
-  }, [buttonState]);
-
-  // set delete longClick flag
+  // longClick logic, do this via useEffect to handle both onMouseDown and onTouchStart being invoked for touch events
   useEffect(() => {
-    if (!deleteActive) return () => { };
+    if (!deleteObj) return () => { };
 
-    const id = setTimeout(() => {
-      canDelete.current = true;
-    }, 1000);
+    id.current = setTimeout(() => {
+      setList((old) => old.filter((itm) => itm.state !== 'delete'));
+      setDeleteObj(undefined);
+    }, 500);
 
-    return () => { clearTimeout(id); };
-  }, [deleteActive]);
+    return () => clearTimeout(id.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deleteObj]);
 
   return (
-    <form onSubmit={onSubmit}>
-      <div className='flex flex-col justify-between gap-2 mb-4 mt-1'>
-        <div className='w-full relative  '>
-          <input
-            className='w-full shadow-2xl rounded-sm p-2 text-black min-h-9 md:min-h-7'
-            type='text'
-            onChange={(e) => setInput((old) => ({ ...old, name: e.target.value }))}
-            value={input.name}
-          />
-          <button
-            type='button'
-            className='text-black absolute z-10 right-0 pr-2 h-full w-10'
-            onClick={(e) => {
-              e.preventDefault();
-              setInput(defaultItem);
-            }}
-          >
-            X
-          </button>
-        </div>
+    <div className='flex flex-col justify-between gap-2 mb-4 mt-1'>
+      <div className='w-full relative  '>
+        <input
+          className='w-full shadow-2xl rounded-sm p-2 text-black min-h-9 md:min-h-7'
+          type='text'
+          onChange={(e) => setInput((old) => ({ ...old, name: e.target.value }))}
+          value={input.name}
+        />
         <button
-          onMouseDown={() => { activateDel(); }}
-          onTouchStart={() => { activateDel(); }}
-          onMouseUp={() => { setDeleteActive(false); }}
-          onTouchEnd={() => { setDeleteActive(false); }}
-          onTouchCancel={() => { setDeleteActive(false); }}
-          onMouseOut={() => { setDeleteActive(false); }}
-          onBlur={() => { setDeleteActive(false); }}
-          type='submit'
-          className={classNames('border-2 border-white rounded-sm min-h-7 capitalize', {
-            [setting.theme.form.show]: buttonState === 'add',
-            [setting.theme.form.edit]: buttonState === 'edit',
-            [setting.theme.form.delete]: buttonState === 'delete',
-            'animate-[pulse_1s]  ease-[cubic-bezier(0.7, 0, 0.84, 0)]': deleteActive,
-          })}
+          type='button'
+          className='text-black absolute z-10 right-0 pr-2 h-full w-10'
+          onClick={() => {
+            setInput(defaultItem);
+          }}
         >
-          {buttonState}
+          X
         </button>
       </div>
-    </form>
+      <button
+        onMouseUp={() => {
+          setDownActive(false);
+        }}
+        onTouchEnd={() => {
+          setDownActive(false);
+        }}
+        // longClick
+        onMouseDown={() => {
+          setDeleteObj(list.filter((v) => v.state === 'delete'));
+          setDownActive(true);
+        }}
+        // longClick
+        onTouchStart={() => {
+          setDeleteObj(list.filter((v) => v.state === 'delete'));
+          setDownActive(true);
+        }}
+        // normal Click
+        onClick={() => {
+          if (!deleteObj) {
+            return;
+          }
+
+          setDeleteObj(undefined);
+
+          let resetInput = false;
+
+          switch (buttonState) {
+            case 'edit': {
+              setList((old) => {
+                const idx = old.findIndex((item) => item.id === input.id);
+                if (idx !== -1) {
+                  resetInput = true;
+                  const tmp = [...old];
+                  tmp[idx] = input;
+                  tmp[idx].state = 'show';
+
+                  return tmp;
+                }
+                return old;
+              });
+              break;
+            }
+            case 'add': {
+              if (input) {
+                setList((old) => [{
+                  name: input.name, quantity: 1, state: 'show', id: `${input}${Date.now()}`,
+                }, ...old]);
+
+                resetInput = true;
+              }
+              break;
+            }
+          }
+
+          if (resetInput) {
+            setInput(defaultItem);
+          }
+        }}
+        className={classNames('border-2 border-white rounded-sm min-h-7 capitalize', {
+          [setting.theme.form.show]: buttonState === 'add',
+          [setting.theme.form.edit]: buttonState === 'edit',
+          [setting.theme.form.delete]: buttonState === 'delete',
+          'animate-[pulse_1s]  ease-[cubic-bezier(0.7, 0, 0.84, 0)]': downActive,
+        })}
+      >
+        {buttonState}
+      </button>
+    </div>
   );
 }
